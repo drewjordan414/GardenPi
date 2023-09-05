8b82835db2c45a00217e283a28085b1414ee4cfc
Drew Jordan <ajordan12@luc.edu>
2023-09-05 10:08:06 -0500

    updates to the python file

--- sensor.py
+++ sensor.py
@@ -12,6 +12,23 @@ ss = adafruit_seesaw(i2c)
 sht = adafruit_sht4x.SHT4x(i2c)
 tsl = adafruit_tsl2591.TSL2591(i2c)
 
+# Create a function to read the temperature from the SHT40
+def read_temp():
+    # read the temperature in Fahrenheit
+    return sht.temperature * 1.8 *32
+
+# Create a function to read the humidity from the SHT40
+def read_humidity():
+    return sht.relative_humidity
+
+# create a function to read the soil moisture from the soil sensor
+def read_soil():
+    return ss.moisture_read()
+
+# function to read the light sensor 
+def read_light():
+    return tsl.lux
+
 # Create a aio instance using the username and key
 aio_usermname = "YOUR_ADAFRUIT_IO_USERNAME"
 aio_key = "YOUR_ADAFRUIT_IO_KEY"
@@ -23,4 +40,15 @@ humiditiy_feed = aio.feeds('humidity')
 soil_feed = aio.feeds('soil')
 light_feed = aio.feeds('light')
 
+# create a guage for the sensors
+temperature_guage = aio.feeds('temperature-guage')
+humidity_gauge = aio.feeds('humidity-guage')
+soil_gauge = aio.feeds('soil-guage')
+light_gauge = aio.feeds('light-gauge')
+
+# create a block for the sensors
+temperature_block = aio.feeds('temperature-block')
+hmidity_block = aio.feeds('humidity-block')
+soil_block = aio.feeds('soil-block')
+light_block = aio.feeds('light-block')
 
