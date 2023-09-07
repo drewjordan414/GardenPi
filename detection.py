bfab16a113d6fd2d0d4f93dece8284e68c3b4ae1
Drew Jordan <ajordan12@luc.edu>
2023-09-06 20:14:25 -0500

    updates

--- detection.py
+++ detection.py
@@ -7,42 +7,215 @@ from torch.utils.data.sampler import SubsetRandomSampler
 import torch.nn as nn
 import torch.nn.functional as F
 from datetime import datetime
-# import library for training on GPU 
-# https://developer.nvidia.com/cuda-downloads
-
-# link to model repo----> https://github.com/manthan89-py/Plant-Disease-Detection/blob/main/Model/Plant%20Disease%20Detection%20Code.ipynb
-
-
-# %load_exit nb_black ----> black formatter matlab
+import os
 
 transform = transforms.Compose(
     [transforms.Resize(255), transforms.CenterCrop(224), transforms.ToTensor()]
 )
+dataset = datasets.ImageFolder("Dataset", transform=transform)
+dataset
+indices = list(range(len(dataset)))
+split = int(np.floor(0.85 * len(dataset)))  # train_size
+validation = int(np.floor(0.70 * split))  # validation
+print(0, validation, split, len(dataset))
+print(f"length of train size :{validation}")
+print(f"length of validation size :{split - validation}")
+print(f"length of test size :{len(dataset)-validation}")
 
-dataset = datasets.ImageFolder("data", transform=transform) # -----> load the dataset for the model 
-
-# split the dataset into training and validation sets
-valid_size = 0.2
-num_train = len(dataset)
-indices = list(range(num_train))
 np.random.shuffle(indices)
-split = int(np.floor(valid_size * num_train))
-train_idx, valid_idx = indices[split:], indices[:split]
-validation = SubsetRandomSampler(valid_idx)
-train = SubsetRandomSampler(train_idx)
+train_indices, validation_indices, test_indices = (
+    indices[:validation],
+    indices[validation:split],
+    indices[split:],
+)
 
-# define samplers for obtaining training and validation batches
-print(0, validation , split, train_idx, valid_idx, len(train_idx), len(valid_idx))
+
+train_sampler = SubsetRandomSampler(train_indices)
+validation_sampler = SubsetRandomSampler(validation_indices)
+test_sampler = SubsetRandomSampler(test_indices)
+
+targets_size = len(dataset.class_to_idx)
+
+# model
+class CNN(nn.Module):
+    def __init__(self, K):
+        super(CNN, self).__init__()
+        self.conv_layers = nn.Sequential(
+            # conv1
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(2),
+            # conv2
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2),
+            # conv3
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2),
+            # conv4
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
+            nn.MaxPool2d(2),
+        )
+
+        self.dense_layers = nn.Sequential(
+            nn.Dropout(0.4),
+            nn.Linear(50176, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.4),
+            nn.Linear(1024, K),
+        )
+
+    def forward(self, X):
+        out = self.conv_layers(X)
+
+        # Flatten
+        out = out.view(-1, 50176)
+
+        # Fully connected
+        out = self.dense_layers(out)
+
+        return out
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+device = "cpu"
+
+model = CNN(targets_size)
+
+model.to(device)
+
+from torchsummary import summary
+
+summary(model, (3, 224, 224))
+
+criterion = nn.CrossEntropyLoss()  # this include softmax + cross entropy loss
+optimizer = torch.optim.Adam(model.parameters())
+
+# batch gradient 
+def batch_gd(model, criterion, train_loader, test_laoder, epochs):
+    train_losses = np.zeros(epochs)
+    test_losses = np.zeros(epochs)
+
+    for e in range(epochs):
+        t0 = datetime.now()
+        train_loss = []
+        for inputs, targets in train_loader:
+            inputs, targets = inputs.to(device), targets.to(device)
+
+            optimizer.zero_grad()
+
+            output = model(inputs)
+
+            loss = criterion(output, targets)
+
+            train_loss.append(loss.item())  # torch to numpy world
+
+            loss.backward()
+            optimizer.step()
+
+        train_loss = np.mean(train_loss)
+
+        validation_loss = []
+
+        for inputs, targets in validation_loader:
+
+            inputs, targets = inputs.to(device), targets.to(device)
+
+            output = model(inputs)
+
+            loss = criterion(output, targets)
+
+            validation_loss.append(loss.item())  # torch to numpy world
+
+        validation_loss = np.mean(validation_loss)
+
+        train_losses[e] = train_loss
+        validation_losses[e] = validation_loss
+
+        dt = datetime.now() - t0
+
+        print(
+            f"Epoch : {e+1}/{epochs} Train_loss:{train_loss:.3f} Test_loss:{validation_loss:.3f} Duration:{dt}"
+        )
+
+    return train_losses, validation_losses
+
+device = "cpu"
+
+batch_size = 64
 train_loader = torch.utils.data.DataLoader(
-    dataset, batch_size=20, sampler=train, num_workers=0
+    dataset, batch_size=batch_size, sampler=train_sampler
+)
+test_loader = torch.utils.data.DataLoader(
+    dataset, batch_size=batch_size, sampler=test_sampler
+)
+validation_loader = torch.utils.data.DataLoader(
+    dataset, batch_size=batch_size, sampler=validation_sampler
 )
 
-print(1, validation , split, train_idx, valid_idx, len(train_idx), len(valid_idx))
-valid_loader = torch._utils.data.DataLoader(
-    dataset, batch_size=20, sampler= validation, num_workers=0
+train_losses, validation_losses = batch_gd(
+    model, criterion, train_loader, validation_loader, 5
 )
 
-print(2, validation , split, train_idx, valid_idx, len(train_idx), len(valid_idx))
-test_loader = torch.utils.data.Dataloader(dataset, batch_size = 20, num_workers = 0)
+# save the model
+# torch.save(model.state_dict() , 'plant_disease_model_1.pt')
 
 
+# load the model
+targets_size = 39
+model = CNN(targets_size)
+model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
+model.eval()
+
+# plot the loss
+plt.plot(train_losses , label = 'train_loss')
+plt.plot(validation_losses , label = 'validation_loss')
+plt.xlabel('No of Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# accuracy 
+def accuracy(loader):
+    n_correct = 0
+    n_total = 0
+
+    for inputs, targets in loader:
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        outputs = model(inputs)
+
+        _, predictions = torch.max(outputs, 1)
+
+        n_correct += (predictions == targets).sum().item()
+        n_total += targets.shape[0]
+
+    acc = n_correct / n_total
+    return acc
+
+train_acc = accuracy(train_loader)
+test_acc = accuracy(test_loader)
+validation_acc = accuracy(validation_loader)
+
+print(
+    f"Train Accuracy : {train_acc}\nTest Accuracy : {test_acc}\nValidation Accuracy : {validation_acc}"
+)
