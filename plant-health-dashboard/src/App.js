ad283efdbbe9ff30375a7c597d5cfa40f1ebe9e0
Drew Jordan <ajordan12@luc.edu>
2023-09-20 17:32:57 -0500

    added in graphs

--- plant-health-dashboard/src/App.js
+++ plant-health-dashboard/src/App.js
@@ -1,5 +1,5 @@
 // import React, { useState, useEffect } from 'react';
-// import { Container, AppBar, Toolbar, Typography, Grid, CssBaseline } from '@material-ui/core';
+// import { Container, AppBar, Toolbar, Typography, Grid, CssBaseline, Card, CardContent, Box } from '@material-ui/core';
 // import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 // import LiveVideo from './components/video';
 // import SensorData from './components/sensor';
@@ -60,29 +60,38 @@
 //           <Grid item xs={12}>
 //             <LiveVideo />
 //           </Grid>
-//           <Grid item xs={6} sm={3}>
-//             <SensorData title="Temperature" value={sensorData.temperature} unit="°F" />
-//           </Grid>
-//           <Grid item xs={6} sm={3}>
-//             <SensorData title="Humidity" value={sensorData.humidity} unit="%" />
-//           </Grid>
-//           <Grid item xs={6} sm={3}>
-//             <SensorData title="Soil Moisture" value={sensorData.soil} />
-//           </Grid>
-//           <Grid item xs={6} sm={3}>
-//             <SensorData title="Light" value={sensorData.light} unit="lux" />
-//           </Grid>
+//           {[
+//             { title: "Temperature", value: sensorData.temperature, unit: "°F" },
+//             { title: "Humidity", value: sensorData.humidity, unit: "%" },
+//             { title: "Soil Moisture", value: sensorData.soil },
+//             { title: "Light", value: sensorData.light, unit: "lux" }
+//           ].map(sensor => (
+//             <Grid item xs={6} sm={3} key={sensor.title}>
+//               <Card>
+//                 <CardContent>
+//                   <SensorData title={sensor.title} value={sensor.value} unit={sensor.unit} />
+//                 </CardContent>
+//               </Card>
+//             </Grid>
+//           ))}
 //         </Grid>
 //       </Container>
+//       <Box style={{ position: 'fixed', right: '1rem', bottom: '1rem' }}>
+//         <Typography variant="body2">
+//           Built with React by Drew
+//         </Typography>
+//       </Box>
 //     </ThemeProvider>
 //   );
 // }
 
 // export default App;
 
+// added in graphs for sensor data
 import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Toolbar, Typography, Grid, CssBaseline, Card, CardContent, Box } from '@material-ui/core';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import LiveVideo from './components/video';
 import SensorData from './components/sensor';
 
@@ -115,7 +124,7 @@ function App() {
   useEffect(() => {
     const fetchSensorData = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/sensor_data');  // Assuming Flask server runs on localhost:5000
+        const response = await fetch('http://localhost:5000/api/sensor_data');
         const data = await response.json();
         setSensorData(data);
       } catch (error) {
@@ -125,6 +134,11 @@ function App() {
 
     fetchSensorData();
   }, []);
+
+  const mockData = [
+    { name: 'Time1', temperature: sensorData.temperature, humidity: sensorData.humidity, soil: sensorData.soil, light: sensorData.light },
+    // Add more data points as required for demonstration purposes
+  ];
 
   return (
     <ThemeProvider theme={theme}>
@@ -142,27 +156,71 @@ function App() {
           <Grid item xs={12}>
             <LiveVideo />
           </Grid>
-          {[
-            { title: "Temperature", value: sensorData.temperature, unit: "°F" },
-            { title: "Humidity", value: sensorData.humidity, unit: "%" },
-            { title: "Soil Moisture", value: sensorData.soil },
-            { title: "Light", value: sensorData.light, unit: "lux" }
-          ].map(sensor => (
-            <Grid item xs={6} sm={3} key={sensor.title}>
-              <Card>
+
+          <Grid item xs={12} sm={6}>
+            {[
+              { title: "Temperature", value: sensorData.temperature, unit: "°F" },
+              { title: "Humidity", value: sensorData.humidity, unit: "%" },
+              { title: "Soil Moisture", value: sensorData.soil },
+              { title: "Light", value: sensorData.light, unit: "lux" }
+            ].map(sensor => (
+              <Card key={sensor.title} style={{ marginBottom: '15px' }}>
                 <CardContent>
                   <SensorData title={sensor.title} value={sensor.value} unit={sensor.unit} />
                 </CardContent>
               </Card>
-            </Grid>
-          ))}
+            ))}
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Card>
+              <CardContent>
+                <Typography variant="h6">Temperature & Humidity</Typography>
+                <LineChart width={400} height={150} data={mockData}>
+                  <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Line type="monotone" dataKey="temperature" stroke="#8884d8" />
+                  <Line type="monotone" dataKey="humidity" stroke="#82ca9d" />
+                </LineChart>
+              </CardContent>
+            </Card>
+
+            <Card style={{ marginTop: '15px' }}>
+              <CardContent>
+                <Typography variant="h6">Soil Moisture</Typography>
+                <LineChart width={400} height={150} data={mockData}>
+                  <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Line type="monotone" dataKey="soil" stroke="#ff7300" />
+                </LineChart>
+              </CardContent>
+            </Card>
+
+            <Card style={{ marginTop: '15px' }}>
+              <CardContent>
+                <Typography variant="h6">Light</Typography>
+                <LineChart width={400} height={150} data={mockData}>
+                  <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Line type="monotone" dataKey="light" stroke="#f34c56" />
+                </LineChart>
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
+
+        <Box style={{ position: 'fixed', right: '1rem', bottom: '1rem' }}>
+          <Typography variant="body2">
+            Built with React by Drew
+          </Typography>
+        </Box>
       </Container>
-      <Box style={{ position: 'fixed', right: '1rem', bottom: '1rem' }}>
-        <Typography variant="body2">
-          Built with React by Drew
-        </Typography>
-      </Box>
     </ThemeProvider>
   );
 }
