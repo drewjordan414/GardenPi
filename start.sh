aecf49a0397041d607d143df29e6844e4b154f55
Drew Jordan <ajordan12@luc.edu>
2023-09-20 17:13:59 -0500

    updates to the start.sh file

--- start.sh
+++ start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Start the Flask backend
-cd /home/drewjordan/GardenPi/backend
+# cd /home/drewjordan/GardenPi/backend
+# relative path
+cd backend
 flask run &
 
 # Start the React frontend
-cd /home/drewjordan/GardenPi/plant-health-dashboard
+# cd /home/drewjordan/GardenPi/plant-health-dashboard
+# realtive path
+cd plant-health-dashboard
 npm start &
 
 # Wait for all child processes to finish
