7b819b4b5e7827210114c066ac73773a1076d00a
Drew Jordan <ajordan12@luc.edu>
2023-09-22 12:51:40 -0500

    updates

--- dashboard/src/components/video.js
+++ dashboard/src/components/video.js
@@ -18,7 +18,7 @@ const LiveVideo = () => {
         <Card style={{ height: '400px' }}>
             <CardContent style={{ height: '100%', padding: 0 }}>
                 <div className="live-video-container" style={{ height: '100%' }}>
-                    <img src="http://localhost:5000/api/video_feed" alt="Live Video Feed" style={{ height: '100%', width: '100%', objectFit: 'cover' }} />
+                    <img src="/api/video_feed" alt="Live Video Feed" style={{ height: '100%', width: '100%', objectFit: 'cover' }} />
                 </div>
             </CardContent>
         </Card>
