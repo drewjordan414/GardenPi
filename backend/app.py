3deeaf9bdcb87d976ca5d735298ea4ab950ee567
Drew Jordan <ajordan12@luc.edu>
2023-09-26 11:25:00 -0500

    updates to video and sesnors data

--- backend/app.py
+++ backend/app.py
@@ -1,127 +1,4 @@
-# from flask import Flask, Response, jsonify
-# import cv2
-# import board
-# import busio 
-# import adafruit_seesaw
-# import adafruit_sht4x
-# import adafruit_tsl2591
-# import tensorflow as tf
-# import numpy as np
-# import tensorflow_hub as hub
-# from flask_cors import CORS
-# import openai
-# import dotenv
-# # Initialize OpenAI API read from env file
-# api_key = dotenv.get_key('.env, API_KEY')
-
-# # Initialize I2C sensors 
-# i2c = busio.I2C(board.SCL, board.SDA)
-# ss = adafruit_seesaw.Seesaw(i2c)
-# sht = adafruit_sht4x.SHT4x(i2c)
-# tsl = adafruit_tsl2591.TSL2591(i2c)
-
-# # Load the saved model
-# model = hub.load("https://tfhub.dev/rishit-dagli/plant-disease/1")
-
-# app = Flask(__name__)
-# CORS(app)  # Handling CORS for local development
-
-# def openai_chat(query):
-#     """Query the OpenAI API and return the response."""
-#     response = openai.Completion.create(
-#         engine="davinci",
-#         prompt=query,
-#         temperature=0.9,
-#         max_tokens=150,
-#         top_p=1,
-#         frequency_penalty=0,
-#         presence_penalty=0.6,
-#         stop=["\n", " Human:", " AI:"]
-#     )
-#     return response.choices[0].text
-
-
-# def read_temp():
-#     """Read the temperature in Fahrenheit from the SHT40."""
-#     return sht.temperature * 1.8 + 32
-
-# def read_humidity():
-#     """Read the humidity from the SHT40."""
-#     return sht.relative_humidity
-
-# def read_soil():
-#     """Read the soil moisture from the soil sensor."""
-#     return ss.moisture_read()
-
-# def read_light():
-#     """Read the light sensor value."""
-#     return tsl.lux
-
-# @app.route('/api/sensor_data')
-# def sensor_data():
-#     """Provide sensor data as a JSON response."""
-#     data = {
-#         "temperature": read_temp(),
-#         "humidity": read_humidity(),
-#         "soil": read_soil(),
-#         "light": read_light()
-#     }
-#     return jsonify(data)
-
-# def preprocess_image(image, target_size=(224, 224)):
-#     """Preprocess the image for the model."""
-#     image = cv2.resize(image, target_size)
-#     image = tf.convert_to_tensor(image)
-#     image = tf.image.convert_image_dtype(image, tf.float32)
-#     return tf.expand_dims(image, axis=0)  # Expand dims to make it a batch of size 1
-
-# def predict_deficiency(image):
-#     """Predict the nutrient deficiency using the loaded model."""
-#     processed_image = preprocess_image(image)
-#     predictions = model(processed_image)
-#     # Assuming model returns class probabilities
-#     predicted_class = tf.argmax(predictions, axis=1).numpy()[0]
-#     return predicted_class
-
-# def gen():
-#     """Generate the video stream."""
-#     cap = cv2.VideoCapture(0)
-#     while True:
-#         ret, frame = cap.read()
-#         if not ret:
-#             print("Can't receive frame (stream end?). Exiting ...")
-#             break
-
-#         # Get the prediction for the frame
-#         result = predict_deficiency(frame)
-        
-#         # Display the result on the frame
-#         label = "Healthy" if result == 0 else "Not Healthy"
-#         color = (0, 255, 0) if label == "Healthy" else (0, 0, 255)
-        
-#         height, width, _ = frame.shape
-#         top_left = (int(width * 0.05), int(height * 0.05))
-#         bottom_right = (int(width * 0.95), int(height * 0.15))
-#         cv2.rectangle(frame, top_left, bottom_right, color, -1)
-#         cv2.putText(frame, label, (int(width * 0.1), int(height * 0.12)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-        
-#         # Convert the frame to JPEG and return
-#         ret, jpeg = cv2.imencode('.jpg', frame)
-#         yield (b'--frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
-
-# @app.route('/api/video_feed')
-# def video_feed():
-#     """Video streaming route."""
-#     return Response(gen(),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000, debug=True)
-
-
-
-# version with no prediction
+# housekeeping, import 
 from flask import Flask, Response, jsonify
 import cv2
 import board
@@ -130,6 +7,7 @@ import adafruit_seesaw
 import adafruit_sht4x
 import adafruit_tsl2591
 from flask_cors import CORS
+import time 
 
 # Initialize I2C sensors 
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -142,11 +20,13 @@ CORS(app)  # Handling CORS for local development
 
 def read_temp():
     """Read the temperature in Fahrenheit from the SHT40."""
-    return sht.temperature * 1.8 + 32
+    temp = sht.temperature * 1.8 + 32
+    return round(temp, 2)
 
 def read_humidity():
     """Read the humidity from the SHT40."""
-    return sht.relative_humidity
+    humidity =  sht.relative_humidity
+    return round(humidity, 2)
 
 def read_soil():
     """Read the soil moisture from the soil sensor."""
@@ -154,7 +34,8 @@ def read_soil():
 
 def read_light():
     """Read the light sensor value."""
-    return tsl.lux
+    light = tsl.lux
+    return round(light, 2)
 
 @app.route('/api/sensor_data')
 def sensor_data():
@@ -167,19 +48,54 @@ def sensor_data():
     }
     return jsonify(data)
 
+# def gen():
+#     """Generate the video stream."""
+#     cap = cv2.VideoCapture(0)
+#     # setup the camera and the video feed 
+#     cap = cv2.VideoCapture(0)
+#     while True:
+#         cap.set(cv2.CAP_PROP_FPS, 30)
+#         ret, frame = cap.read()
+#         if not ret:
+#             print("Can't receive frame (stream end?). Exiting ...")
+#             break 
+
+    # while True:
+    #     # set the frame rate to 30fps 
+    #     cap.set(cv2.CAP_PROP_FPS, 30)
+    #     ret, frame = cap.read()
+    #     if not ret:
+    #         print("Can't receive frame (stream end?). Exiting ...")
+    #         break
+        
+    #     # Convert the frame to JPEG and return
+    #     ret, jpeg = cv2.imencode('.jpg', frame)
+    #     yield (b'--frame\r\n'
+    #            b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
 def gen():
     """Generate the video stream."""
     cap = cv2.VideoCapture(0)
+    
+    # Reduce resolution for faster processing (e.g., set to 640x480)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
     while True:
+        start_time = time.time()
+
         ret, frame = cap.read()
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
-        
-        # Convert the frame to JPEG and return
-        ret, jpeg = cv2.imencode('.jpg', frame)
+
+        # Increase JPEG compression for smaller frame size, but be aware this might reduce quality
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 70]  
+        ret, jpeg = cv2.imencode('.jpg', frame, encode_param)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+
+        # Use a fixed time delay to attempt a more consistent frame rate
+        time.sleep(0.0333)
 
 @app.route('/api/video_feed')
 def video_feed():
